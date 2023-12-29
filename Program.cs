b12517c27a725f4dd16b45ab32a6a4d9c0beb40d
Leonardo Costa <leonardo.everton@mblabs.com.br>
2023-12-29 17:46:02 -0300

    chore: add identity

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
-
 using IWantApp.Endpoints.Categories;
 using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
+builder.Services.AddSqlServer<ApplicationDbContext>(
+    builder.Configuration["ConnectionString:IWantDb"]);
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+  options.Password.RequireNonAlphanumeric = false;
+  options.Password.RequireDigit = false;
+  options.Password.RequireUppercase = false;
+  options.Password.RequireLowercase = false;
+  options.Password.RequiredLength = 3;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
